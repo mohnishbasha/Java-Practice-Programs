c35a275ccff1c765b887163a3d027dd8bda912f2
Mohinish <your.email@sap.com>
2016-09-05 15:00:51 -0700

    Adding impl2 - singleton pattern

--- org/designpatterns/creational/singleton/SingleSpooler.java
+++ org/designpatterns/creational/singleton/SingleSpooler.java
@@ -1,41 +1,52 @@
 package org.designpatterns.creational.singleton;
 
-class SingletonException extends RuntimeException{
-	public SingletonException(){
-		super();
+class SingletonException extends RuntimeException {
+
+	public SingletonException() {
+        super();
 	}
-	public SingletonException(String s){
-		super(s);
+
+	public SingletonException(String s) {
+        super(s);
 	}
 }
 
-class Spooler{
-	static boolean instance_flag= false;
-	public Spooler() throws SingletonException{
+class Spooler {
+
+	static boolean instance_flag = false;
+
+	public Spooler() throws SingletonException {
+
 		if(instance_flag)
 			throw new SingletonException("Only one allowed");
 		else
 			instance_flag=true;
-		System.out.println("Printer Opened");
+
+        System.out.println("Printer Opened");
 	}
 }
 
 
 
 public class SingleSpooler {
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
+
 		Spooler pr1, pr2;
 		System.out.println("Opening one spooler");
-		try{
+
+        try {
 			pr1  = new Spooler();
 		}
-		catch(SingletonException e){
+		catch(SingletonException e) {
 			System.out.println(e.getMessage());
 		}
-		try{
+
+		try {
 			pr2  = new Spooler();
 		}
-		catch(SingletonException e){
+
+		catch(SingletonException e) {
 			System.out.println(e.getMessage());
 		}
 	}
