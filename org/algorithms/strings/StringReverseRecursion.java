026b0165042ec4434d532f84179ad56572e8e0e4
Mohinish <your.email@sap.com>
2016-09-30 22:34:14 -0700

    Updating new programs.

--- org/algorithms/strings/StringReverseRecursion.java
+++ org/algorithms/strings/StringReverseRecursion.java
@@ -27,6 +27,27 @@ public class StringReverseRecursion {
 		}
 	}
 
+
+	public static void reverseN() {
+
+		String input = "Be in present";
+		char[] temparray= input.toCharArray();
+
+		int left,right=0;
+		right=temparray.length-1;
+
+		for (left=0; left < right ; left++ ,right--)
+		{
+			// Swap values of left and right
+			char temp = temparray[left];
+			temparray[left] = temparray[right];
+			temparray[right]=temp;
+		}
+
+		for (char c : temparray)
+			System.out.print(c);
+	}
+
 	public static void main(String[] args) {
 		String str = "What's going on?";
 		System.out.println(StringReverseRecursion.reverse(str));
